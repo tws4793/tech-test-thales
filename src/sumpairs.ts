98218591dcf1a8d22a6fef7d805296602a430948
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2022-06-11 00:14:47 +0800

    add naiive algorithm

--- src/sumpairs.ts
+++ src/sumpairs.ts
@@ -1,5 +1,15 @@
-const sumPairs = (integers: Array<number>, sum: number): Array<Array<number>> => {
-  return [[0,0]]
+const sumPair = (integers: Array<number>, sum: number): Array<Array<number>> => {
+  let output: Array<Array<number>> = []
+
+  for(let i = 0; i < integers.length; i++){
+    for(let j = i + 1; j < integers.length; j++){
+      if(integers[i] + integers[j] === sum){
+        output.push([integers[i], integers[j]])
+      }
+    }
+  }
+
+  return output
 }
 
-export default sumPairs
+export default sumPair
