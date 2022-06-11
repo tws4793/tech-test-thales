2f81f60b66fb95a53c9c349781c86621689380ea
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2022-06-11 11:59:50 +0800

    add documentation

--- src/sumpairs.ts
+++ src/sumpairs.ts
@@ -1,3 +1,19 @@
+/**
+ * sumpairs.ts
+ */
+
+/**
+ * Finds the pair of sums that are equal to the expected sum.
+ *
+ * @param integers - array of integers
+ * @param sum - expected sum
+ * @returns an array of pairs of sums that are equal to the expected sum
+ *
+ * @example
+ * For an unsorted array `[5, 4, -3, 1, 2, 9]` with sum 6,
+ * it should return `[[5,1],[4,2],[-3,9]] because these pairs
+ * add up to 6.
+ */
 const sumPair = (integers: Array<number>, sum: number): Array<Array<number>> => {
   let output: Array<Array<number>> = []
 
