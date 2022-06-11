68dc055a2e99125ac445cadbf2bb1d01b3148f8c
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2022-06-11 11:53:09 +0800

    write test cases including mocks

--- tests/sumpairs.test.ts
+++ tests/sumpairs.test.ts
@@ -1,17 +1,37 @@
 import sumPairs from '@src/sumpairs'
-import testCases from './sumpairs.json'
+import testCases, { TestCase } from '@tests/testcases'
 
-type TestCase = {
-  input: Array<number>;
-  sum: number;
-};
+const TIMEOUT: number = 1000
 
-describe('sumPairs function is correct', () => {
-  testCases.forEach((test: TestCase) => {
-    const result: Array<Array<number>> = sumPairs(test.input, test.sum)
+describe(`sumPairs function is correct`, () => {
+  testCases.forEach((test: TestCase, index: number) => {
+    describe(`Test Case ${index + 1}: sum ${test.sum}, array ${test.input}`, () => {
+      let result: Array<Array<number>>
+      beforeAll(() => {
+        result = sumPairs(test.input, test.sum)
+      }, TIMEOUT)
 
-    it.todo('elements should be a pair')
-    it.todo('elements in the pair should be present in original array')
-    it.todo('elements in each array sum to n')
+      it(`all elements should be a pair`, () => {
+        const arrayLengths: Array<number> = result.map(expectedPair => expectedPair.length)
+        const isPair = (length: number): boolean => length == 2
+        const arePairs: boolean = arrayLengths.every(isPair)
+
+        expect(arePairs).toBe(true)
+      })
+
+      it(`all elements in the pair should be present in original array`, () => {
+        result.forEach(pair => {
+          expect(test.input).toEqual(expect.arrayContaining(pair))
+        })
+      })
+
+      it(`all elements in each array sum to ${test.sum}`, () => {
+        const arraySums: Array<number> = result.map(x => x[0] + x[1])
+        const isSame = (n: number): boolean => n == test.sum
+        const areSameSum: boolean = arraySums.every(isSame)
+
+        expect(areSameSum).toBe(true)
+      })
+    })
   })
 })
---
+++ tests/testcases.ts
@@ -0,0 +1,53 @@
+import testCases from './sumpairs.json'
+
+export type TestCase = {
+  input: Array<number>;
+  sum: number;
+};
+
+const noMockTestCases = 16
+const sizeMockTestCases = 10000
+const minNumber = -100000
+const maxNumber = 100000
+
+/**
+ * Returns an array of integers that are randomised
+ *
+ * @param min - minimum value of the integer
+ * @param max - maximum value of the integer
+ * @returns a random number
+ */
+const generateRandomNumber = (min: number = 0, max: number = 10): number => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  const randomNumber: number = Math.random() * (max - min + 1) + min
+  return Math.floor(randomNumber)
+}
+
+/**
+ * Returns an array of integers that are randomised
+ *
+ * @param size - the size of the array
+ * @param min - minimum value of the integer
+ * @param max - maximum value of the integer
+ * @returns an array of integers
+ */
+const generateIntegers = (size: number, min: number, max: number): Array<number> => Array.from({length: size}, () => generateRandomNumber(min, max))
+
+/**
+ * Returns a new mock test case
+ *
+ * @param size - the size of the array
+ * @param min - minimum value of the integer
+ * @param max - maximum value of the integer
+ * @returns a mock test case
+ */
+const generateTestCase = (size: number, min: number, max: number): TestCase => ({
+  'input': generateIntegers(size, min, max),
+  'sum': generateRandomNumber(min, max)
+})
+
+const generateMockTestCases: Array<TestCase> = Array.from({length: noMockTestCases}, () => generateTestCase(sizeMockTestCases, minNumber, maxNumber))
+testCases.push(...generateMockTestCases)
+
+export default testCases
